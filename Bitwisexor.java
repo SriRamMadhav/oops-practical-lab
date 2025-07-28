d388c164cd3d697840f2057637628fdfed3ec150
SriRamMadhav <mukkamalasrirammadhav@gmail.com>
2025-07-28 20:11:48 +0530

    Add files via upload

---
+++ Bitwisexor.java
@@ -0,0 +1,26 @@
+package oops_practical;
+
+import java.util.Scanner;
+
+public class Bitwisexor {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("enter the first number or integer(a): ");
+		int a = input.nextInt();
+		
+		System.out.print("enter the second number or integer(b)");
+		int b = input.nextInt();
+		
+		int c = 0;
+		
+		c = a^b;
+		System.out.println("Result of a^b = " +c);
+		
+		c = ~a;
+		System.out.println("Result of ~a = " + c);
+		
+		input.close();
+	}
+	
+}
