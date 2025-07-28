81a136fa04bfb7610ee43e931e1d4686791c95df
SriRamMadhav <mukkamalasrirammadhav@gmail.com>
2025-07-28 21:02:13 +0530

    Add files via upload

---
+++ RelationalOP.java
@@ -0,0 +1,25 @@
+package oops_practical;
+
+import java.util.Scanner;
+
+public class RelationalOP {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("enter 1st integer (a)");
+		int a = input.nextInt();
+		
+		System.out.print("enter 2nd integer (b)");
+		int b = input.nextInt();
+		
+		System.out.println("a is "+a+"and b is "+b);
+		System.out.println("is a==b?" + (a==b));
+		System.out.println("is a!=b? " + (a!=b));
+		System.out.println("is a>b? " + (a>b));
+		System.out.println("is a<b? " + (a<b));
+		System.out.println("is a>=b?" + (a>=b));
+		System.out.println("is a<=b?" + (a<=b));
+		
+		input.close();
+	}
+}
