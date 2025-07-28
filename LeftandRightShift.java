4e45712a51f7a73ad0eaa81c62707cfc9748ff81
SriRamMadhav <mukkamalasrirammadhav@gmail.com>
2025-07-28 20:20:15 +0530

    Add files via upload

---
+++ LeftandRightShift.java
@@ -0,0 +1,27 @@
+package oops_practical;
+
+import java.util.Scanner;
+
+public class LeftandRightShift {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("enter a number (a):");
+		int a = input.nextInt();
+		
+		int c = 0;
+		
+		c = a<<2;
+		System.out.println("result of a<<2 = "+c);
+		
+		c=a>>2;
+		System.out.println("result of a>>2 = "+c);
+		
+		c=a>>>2;
+		System.out.println("result of a>>>2 = "+c);
+		
+		input.close();
+	}
+	
+
+}
